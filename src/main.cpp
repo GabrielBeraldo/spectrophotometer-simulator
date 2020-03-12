43b36279ce0d17c32651f4ff4d88cc9279dc8b31
gabriel beraldo <gabrielgbst@hotmail.com>
2020-03-12 16:37:57 -0300

    working update

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,25 @@
 
 //simModePin high: input value on sinSource pin
 //simModePin low: random generated value between min and max defined below
-#define simModePin     A7 
-#define simSource      A5
+#define simModePin     2 
+#define simSource      A0
 
-#define randomRange_min 95
-#define randomRange_max 105
 
 //high: transmitance
 //low: absorbance
-#define measureUnitPin A6
+#define measureUnitPin 3
 
 #define transmitance " %T"
 #define absorbance " Abs"
 
 void setup() {
   //initialize inputs
-  pinMode(simModePin, INPUT);
+  pinMode(simModePin, INPUT_PULLUP);
   pinMode(simSource, INPUT);
+  pinMode(measureUnitPin, INPUT_PULLUP);
 
-  digitalWrite(simModePin, LOW);
-  digitalWrite(measureUnitPin, LOW);
+  //digitalWrite(simModePin, LOW);
+  //digitalWrite(measureUnitPin, LOW);
 
   //initialize serial
   Serial.begin(9600, SERIAL_7N1);
@@ -32,8 +31,9 @@ void setup() {
 
 void loop() {
   //choose mode 
-  bool autoGenerate = !digitalRead(simModePin);
-  bool unitPinState = digitalRead(measureUnitPin);
+  bool autoGenerate = digitalRead(simModePin);
+  bool unitPinState = !digitalRead(measureUnitPin);
+
   String reading = "";
 
   String unit = unitPinState ? transmitance : absorbance;
@@ -41,23 +41,24 @@ void loop() {
   //generate data
   float rawValue = 
         autoGenerate ? 
-        random(randomRange_min*1000, randomRange_max*1000)/1000:
-        map(analogRead(simSource), 0, 1024, 0, 200000)/1000;
+        random((92000), (101000))/1000:
+        map(analogRead(simSource), 0, 1024, 10, 200000)/1000;
 
   float read = 
         unitPinState ?
         rawValue : //transmitance value
         (2-log10(rawValue)); //transmitance
   
-  /*
+  
   //debug values
-  reading = 
-          "raw value: " + 
-          String(rawValue)+ 
-          " reading: " + 
-          (unitPinState ? String(read, 2) : String(read, 3)) + 
-          unit;
-  */
+  //reading = 
+  //        "raw value: " + 
+  //        String(rawValue)+ 
+  //        " reading: " + 
+  //        (unitPinState ? String(read, 2) : String(read, 3)) + 
+  //        unit;
+  
+ 
   reading =
           "  " + 
           (unitPinState ? String(read, 2) : String(read, 3)) + 
